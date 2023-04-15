ba151bee81252fcbbc35bb569418433e00ea4951
Lars Lemke <contact@lars-lemke.com>
2023-04-15 18:35:34 +0200

    TDS added (#1)
    * added TDS sensor support
    
    * TDS fixed

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,34 @@ Ds18b20 ds18b20(PC_0);
 Ticker tickerTemp;
 float temp;
 
+AnalogIn TDS(PB_0); //TDS Sensor
+Ticker tickerTDS;
+float tdsValue;
+
 
 void getTemp() {
     ds18b20.start();
     wait_ms(750);
     temp = ds18b20.readTemperature();
+    //printf("Temperatur: %.2f Grad Celsius\n", temp);
+}
+
+void getTDS() {
+    float voltage = TDS.read() * 3.3;
+    tdsValue = voltage / 1024 * 1000;
+    //printf("TDS Value: %f ppm\n", tdsValue);
 }
 
 int main() {
     //Setup
     tickerTemp.attach(&getTemp, 1);     //Intervall sollte nicht zu klein sein, da sonst die Messung nicht fertig ist
-
+    tickerTDS.attach(&getTDS, 1);     //Intervall sollte nicht zu klein sein, da sonst die Messung nicht fertig ist
     
     while(1) {
         // Temperatur auf der Konsole ausgeben
         printf("Temperatur: %.2f Grad Celsius\n", temp);
-        
+        // TDS auf der Konsole ausgeben
+        printf("TDS Value: %f ppm\n", tdsValue);
         // Eine Sekunde warten
         wait(1);
     }
