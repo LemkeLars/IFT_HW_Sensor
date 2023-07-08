03b048431d78b3bd249fbeb6ab30091f6999525d
CrazyBuster0864 <lars-lemke@gmx.net>
2023-07-08 22:09:38 +0200

    added PH support

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 //Setup
 #define AVERAGE_INTERVAL 60                        // Average over 60 seconds
 #define SAMPLE_INTERVAL 15*60                      // Sample every 15 minutes
-#define SensorAnount 2
+#define SensorAnount 4
 
 Ticker oneMinuteTicker;
 Ticker xMinuteTicker;
@@ -14,7 +14,7 @@ Serial bluetooth(PA_9, PA_10);
 Serial pc(USBTX, USBRX);
 
 
-Ds18b20 ds18b20(PC_0);
+Ds18b20 ds18b20(PC_0);                              //Temperature Sensor      
 float temp;
 float tempSamples[AVERAGE_INTERVAL];
 int tempSampleIndex = 0;
@@ -26,6 +26,19 @@ int tdsSampleIndex = 0;
 
 Ticker ticker;
 char bluetoothData;
+int tickerCount = 0;
+
+AnalogIn ph(PA_1);                                  //pH Sensor              
+float phValue;
+float phSamples[AVERAGE_INTERVAL];
+int phSampleIndex = 0; 
+
+/* DigitalOut trig(PA_13);                             //ultrasonic Sensor    
+InterruptIn echo(PA_14);                               //Breaks everything         
+Timer echoTimer; */
+float abstand = 0;
+float levelSamples[AVERAGE_INTERVAL];
+int levelSampleIndex = 0;
 
 
 void bluetoothReceive() {
@@ -56,6 +69,17 @@ float average(float value[AVERAGE_INTERVAL], int index) {
     return sum / index;
 }
 
+float getLevel() {
+    printf("Abstand: %f cm\n\r", abstand);
+    return abstand;
+}
+
+float getPH() {
+    float phValue = ph.read()*14;
+    printf("pH Value: %.2f\n\r", phValue);
+    return phValue;
+}
+
 float getTemp() {
     ds18b20.start();
     wait_ms(750);
@@ -72,18 +96,25 @@ float getTDS() {
 
 void readSensor() { 
     // Reset index if necessary
-    if ((tempSampleIndex >= AVERAGE_INTERVAL) | (tdsSampleIndex >= AVERAGE_INTERVAL)) {
+    if ((tempSampleIndex >= AVERAGE_INTERVAL) | (tdsSampleIndex >= AVERAGE_INTERVAL) | (phSampleIndex >= AVERAGE_INTERVAL) | (levelSampleIndex >= AVERAGE_INTERVAL)) {
         tempSampleIndex = 0;
         tdsSampleIndex = 0;
+        phSampleIndex = 0;
+        levelSampleIndex = 0;
+        // Detach ticker
         oneMinuteTicker.detach();
         // Calculate average
         float tempAverage = average(tempSamples, AVERAGE_INTERVAL);
         float tdsAverage = average(tdsSamples, AVERAGE_INTERVAL);
+        float phAverage = average(phSamples, AVERAGE_INTERVAL);
+        float levelAverage = average(levelSamples, AVERAGE_INTERVAL);
         // Print average
         printf("Average temperature: %.2f Grad Celsius\n\r", tempAverage);
         printf("Average TDS Value: %f ppm\n\r", tdsAverage);
+        printf("Average pH Value: %f\n\r", phAverage);
+        printf("Average level: %f cm\n\r", levelAverage);
         // Send average to bluetooth
-        float data[SensorAnount] = {tempAverage, tdsAverage};
+        float data[SensorAnount] = {tempAverage, tdsAverage, phAverage, levelAverage};
         // print the data to the serial port
         for (int i = 0; i < SensorAnount; i++) {
             pc.printf("Data: %f\r\n", data[i]);
@@ -93,9 +124,13 @@ void readSensor() {
         // Read sensor
         tempSamples[tempSampleIndex] = getTemp();
         tdsSamples[tdsSampleIndex] = getTDS();
+        phSamples[phSampleIndex] = getPH();
+        levelSamples[levelSampleIndex] = getLevel();
         // Increment index
         tempSampleIndex++;
         tdsSampleIndex++;
+        phSampleIndex++;
+        levelSampleIndex++;
     }
 }
 
